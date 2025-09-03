58e6760f459617c1c6d56155a2e3e57539137268
Jesus Flores Espinoza <jesus.flores.esp@gmail.com>
2025-09-02 19:25:55 -0600

    cambio en la deteccion de precipitaciones, usando weatherCodes en lugar de probabilidad

--- weather_api.py
+++ weather_api.py
@@ -1,5 +1,5 @@
 import requests
-from config import API_KEY, LAT, LON, WEATHER_CODE, DANGEROUS_CODES
+from config import API_KEY, LAT, LON
 
 def get_realtime():
     url = "https://api.tomorrow.io/v4/weather/realtime"
